c4c28721a03fe1d1ff7016f1fb4bc5ca98b2f886
张文皓 <zhangwh754@163.com>
2022-12-07 11:59:26 +0800

    ✨ feat: test

---
+++ src/views/1-Test.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h2 v-for="item in 5" :key="item">{{ item }}</h2>
+</template>
+
+<style scoped lang="scss">
+h2 {
+  margin: 10px;
+  height: 30px;
+  border: 1px solid #ccc;
+}
+</style>
