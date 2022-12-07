76be329584fc25b67f67712ac08e12cff808baf2
张文皓 <zhangwh754@163.com>
2022-12-07 12:00:17 +0800

    ✨ feat: 递归组件

---
+++ src/components/Tree.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+type Tree = {
+  name: string
+  checked: boolean
+  children?: Tree[]
+}
+
+type Props = {
+  data?: Tree[]
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="tree" v-for="item in data" :key="item.name">
+    <el-checkbox v-model="item.checked" :label="item.name" size="large" />
+    <Tree v-if="item?.children" :data="item.children" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.tree {
+  margin-left: 20px;
+  .el-checkbox {
+    padding-left: 20px;
+    display: flex;
+    align-items: center;
+    &:hover {
+      background-color: lightblue;
+    }
+  }
+}
+</style>
---
+++ src/views/3-Tree.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import Tree from '@/components/Tree.vue'
+import { reactive } from 'vue'
+type TreeType = {
+  name: string
+  checked: boolean
+  children?: TreeType[]
+}
+
+const data = reactive<TreeType[]>([
+  {
+    name: '1',
+    checked: false,
+    children: [
+      {
+        name: '1-1',
+        checked: false
+      }
+    ]
+  },
+  {
+    name: '2',
+    checked: true
+  },
+  {
+    name: '3',
+    checked: false,
+    children: [
+      {
+        name: '3-1',
+        checked: false,
+        children: [
+          {
+            name: '3-1-1',
+            checked: true
+          },
+          {
+            name: '3-1-2',
+            checked: false
+          },
+          {
+            name: '3-1-3',
+            checked: true
+          }
+        ]
+      },
+      {
+        name: '3-2',
+        checked: false
+      }
+    ]
+  }
+])
+</script>
+
+<template>
+  <Tree :data="data"></Tree>
+</template>
+
+<style scoped lang="scss"></style>
