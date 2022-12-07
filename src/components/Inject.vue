63a47661e7ad0764e7e3eabc86825abda114d09a
张文皓 <zhangwh754@163.com>
2022-12-07 13:34:12 +0800

    ✨ feat: 依赖注入(provide|inject)

---
+++ src/components/Inject.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { inject, type Ref } from 'vue'
+
+const color = inject<Ref<string>>('color')
+</script>
+
+<template>
+  <h2>Inject子组件</h2>
+  <el-radio-group v-model="color">
+    <el-radio label="red" size="large">child red</el-radio>
+    <el-radio label="yellow" size="large">child yellow</el-radio>
+    <el-radio label="pink" size="large">child pink</el-radio>
+  </el-radio-group>
+  <div id="box"></div>
+</template>
+
+<style scoped lang="scss">
+#box {
+  width: 100px;
+  height: 100px;
+  background-color: v-bind(color);
+}
+</style>
---
+++ src/views/10-Provide.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { provide, readonly, ref } from 'vue'
+import Inject from '@/components/Inject.vue'
+
+const color = ref('red')
+
+provide('color', readonly(color))
+</script>
+
+<template>
+  <el-radio-group v-model="color">
+    <el-radio label="red" size="large">red</el-radio>
+    <el-radio label="yellow" size="large">yellow</el-radio>
+    <el-radio label="pink" size="large">pink</el-radio>
+  </el-radio-group>
+  <h2>provide父组件</h2>
+  <div id="box"></div>
+  <hr />
+  <Inject></Inject>
+</template>
+
+<style scoped lang="scss">
+#box {
+  width: 100px;
+  height: 100px;
+  background-color: v-bind(color);
+}
+</style>
