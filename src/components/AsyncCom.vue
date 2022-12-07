24cb8a9b2b1712096494ac7a18d1d3692a8e3ebc
张文皓 <zhangwh754@163.com>
2022-12-07 12:01:07 +0800

    ✨ feat: 异步组件

---
+++ src/components/AsyncCom.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+type Data = {
+  name: string
+  age: number
+  desc: string
+}
+
+const data: Data = {
+  name: 'zwh',
+  age: 18,
+  desc: 'hello world'
+}
+
+const getData = (): Promise<Data> => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(data)
+    }, 2000)
+  })
+}
+
+const asyncData = await getData()
+</script>
+
+<template>
+  <div class="sync">
+    <div class="sync-content">
+      <div class="sync-info">
+        <div>{{ asyncData.name }}</div>
+        <div>{{ asyncData.age }}</div>
+      </div>
+    </div>
+    <hr />
+    <div>{{ asyncData.desc }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/views/5-AsyncCom.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { defineAsyncComponent } from 'vue'
+const Sync = defineAsyncComponent(() => import('@/components/AsyncCom.vue'))
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <Sync></Sync>
+    </template>
+    <template #fallback>
+      <div>loading...</div>
+    </template>
+  </Suspense>
+</template>
+
+<style scoped lang="scss"></style>
